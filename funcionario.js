b6b3d573370785c206b72abc00100fc299d12a7c
GustavoKasaki <gmkasaki@gmail.com>
2024-10-23 13:35:00 -0300

    Aula 5 - Aplique encapsulamento

--- funcionario.js
+++ funcionario.js
@@ -10,11 +10,25 @@ function Pessoa(nome) {
 
 function Funcionario(nome, cargo, salario) {
     this.cargo = cargo;
-    this.salario = salario;
+    // this.salario = salario;
+    let _salario = salario; //'_' antes do atributo torna-o privado
+
+    this.getSalario = function() {
+        return `O salário de ${this.nome} é R$ ${_salario}`;
+    }
+
+    this.setSalario = function(valor) {
+        if (typeof valor === 'number') { //verifica se 'valor' é um número e não uma string
+            _salario = valor;
+        }
+    }
 
     Pessoa.call(this, nome); //acessa as informações do objeto 'Pessoa'
 }
 
-const funcionario1 = new Funcionario('Maria', 'Dev Front-End', 'R$ 3.000,00');
-funcionario1.dizOi();
-funcionario1.dizCargo();
+const funcionario1 = new Funcionario('Maria', 'Dev Front-End', '5000');
+
+funcionario1.setSalario('mil');
+
+console.log(funcionario1);
+console.log(funcionario1.getSalario());
