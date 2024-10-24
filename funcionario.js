5e4b94fe2c7568abab89010202485353dd5d6c64
GustavoKasaki <gmkasaki@gmail.com>
2024-10-24 13:27:39 -0300

    Aula 6 - Aplique o polimorfismo

--- funcionario.js
+++ funcionario.js
@@ -14,7 +14,7 @@ function Funcionario(nome, cargo, salario) {
     let _salario = salario; //'_' antes do atributo torna-o privado
 
     this.getSalario = function() {
-        return `O salário de ${this.nome} é R$ ${_salario}`;
+        return _salario;
     }
 
     this.setSalario = function(valor) {
@@ -23,12 +23,43 @@ function Funcionario(nome, cargo, salario) {
         }
     }
 
+    this.aumento = function() {
+        const novoSalario = _salario * 1.1;
+        _salario = novoSalario;
+    }
+
     Pessoa.call(this, nome); //acessa as informações do objeto 'Pessoa'
 }
 
+function Estagiario(nome) {
+    Funcionario.call(this, nome, 'Estagiário', '2000');
+
+    this.aumento = function() {
+        const novoSalario = this.getSalario() * 1.07;
+        this.setSalario(novoSalario);
+    }
+}
+
+function Gerente(nome) {
+    Funcionario.call(this, nome, 'Gerente', '10000');
+
+    this.aumento = function() {
+        const novoSalario = this.getSalario() * 1.15;
+        this.setSalario(novoSalario);
+    }
+}
+
 const funcionario1 = new Funcionario('Maria', 'Dev Front-End', '5000');
+const funcionario2 = new Estagiario('Pedro');
+const funcionario3 = new Gerente('Paula');
 
 funcionario1.setSalario('mil');
 
-console.log(funcionario1);
+funcionario1.aumento();
 console.log(funcionario1.getSalario());
+
+funcionario2.aumento();
+console.log(funcionario2.getSalario());
+
+funcionario3.aumento();
+console.log(funcionario3.getSalario());
