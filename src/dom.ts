41fa1c3001ec19aafa7a705b84b479f1e742eea0
GustavoKasaki <gmkasaki@gmail.com>
2024-11-21 13:20:23 -0300

    Aula 8 - Integre Typescript com DOM

---
+++ src/dom.ts
@@ -0,0 +1,8 @@
+// É muito trabalhoso utilizar o Typescript para manipulação do DOM. Geralmente é utilizado em aplicações WEB.
+
+const campoNome = document.getElementById('nome');
+const form = document.getElementsByTagName('form');
+const btnEnviar = document.getElementById('enviar');
+
+btnEnviar?.addEventListener('click', (e:MouseEvent) => {
+})
