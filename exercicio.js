d098a937543249907a319486aab31fd3b1af1497
GustavoKasaki <gmkasaki@gmail.com>
2024-11-21 20:18:24 -0300

    Commit inicial

---
+++ exercicio.js
@@ -0,0 +1,9 @@
+"use strict";
+// Exercício do módulo 26 - Typescript
+// Gustavo Kasaki - https://github.com/GustavoKasaki
+// Primeira função: multiplicação de dois números
+const multiplicar = (x, y) => x * y;
+console.log(multiplicar(2, 5)); // Teste
+// Segunda função: Saudação (concatenação de palavras)
+const saudacao = (nome) => 'Olá ' + nome;
+console.log(saudacao('gustavo')); // Teste
---
+++ exercicio.ts
@@ -0,0 +1,10 @@
+// Exercício do módulo 26 - Typescript
+// Gustavo Kasaki - https://github.com/GustavoKasaki
+
+// Primeira função: multiplicação de dois números
+const multiplicar = (x: number, y: number): number => x * y;
+console.log(multiplicar(2, 5)); // Teste
+
+// Segunda função: Saudação (concatenação de palavras)
+const saudacao = (nome: string) => 'Olá ' + nome;
+console.log(saudacao('gustavo')); // Teste
