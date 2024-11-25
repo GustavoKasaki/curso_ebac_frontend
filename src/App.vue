2efadc6357ef8997aa0e2217de370cdc81c0bddf
GustavoKasaki <gmkasaki@gmail.com>
2024-11-25 13:26:44 -0300

    Aula 4 - Utilize eventos para manipular dados

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { reactive } from 'vue';
+
   const nome = 'Gustavo';
   const meuObj = {
     nome: 'Gustavo',
@@ -18,6 +20,25 @@
   const gostaImagem2 = true;
 
   const estaAutorizado = true;
+
+  // let contador = 0;
+  const estado = reactive({
+    contador: 0,
+    email: '',
+  })
+
+  function incrementar() {
+    estado.contador++;
+  }
+
+  function decrementar() {
+    estado.contador--;
+  }
+
+  function alteraEmail(evento) {
+    estado.email = evento.target.value;
+  }
+
 </script>
 
 <template>
@@ -31,6 +52,19 @@
   <h1 v-else>Não autorizado</h1>
   
   <button :disabled="botaoEstaDesabilitado">Enviar mensagem</button>
+
+  <br>
+  <hr>
+
+  {{ estado.contador }}
+  <button @click="incrementar"type="button">+</button>
+  <button @click="decrementar" type="button">-</button>
+
+  <br>
+  <hr>
+
+  {{ estado.email }}
+  <input type="email" @keyup="alteraEmail">
 </template>
 
 <style scoped>
