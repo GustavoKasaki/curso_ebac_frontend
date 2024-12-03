65dbe454d900c5e7a43d513741f87531f70672d6
GustavoKasaki <gmkasaki@gmail.com>
2024-12-02 21:21:48 -0300

    Aula 3 - Manipule estado e eventos

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,12 @@
 import Perfil from "./components/Perfil";
+import Formulario from "./components/Formulario";
 
 function App() {
 
-  const nome = 'react';
-
-  function retornaNome() {
-    return nome;
-  }
-
-  const pessoa = {
-    nome: 'Maria'
-  }
-
-  let estaDeDia = true;
-
   return (
     <>
-      <Perfil />
-      <h1>Olá, {pessoa.nome}</h1>
-      <h2>Subtítulo</h2>
-      {estaDeDia ? 'Bom dia' : 'Boa tarde'}
+      <Perfil nome='Gustavo' endereco='https://github.com/GustavoKasaki.png' />
+      <Formulario />
     </>
   )
 }
--- src/components/Perfil/index.jsx
+++ src/components/Perfil/index.jsx
@@ -1,15 +1,10 @@
 import './perfil.css'
 
-const Perfil = () => {
-    const usuario = {
-        nome: 'Gustavo',
-        avatar: 'https://github.com/GustavoKasaki.png'
-    }
-
+const Perfil = ({ endereco, nome }) => {
     return (
         <div>
-            <img className='perfil-avatar' src={ usuario.avatar }/>
-            <h3 className='perfil-titulo'>{ usuario.nome }</h3>
+            <img className='perfil-avatar' src={ endereco }/>
+            <h3 className='perfil-titulo'>{ nome }</h3>
         </div>
     )
 }
